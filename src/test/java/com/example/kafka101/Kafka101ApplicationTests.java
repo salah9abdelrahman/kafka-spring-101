55caef7172cceb7345d0adbde61b4a4a89b6f342
salah9abdelrahman <salah9abdelrahman@outlook.com>
2022-06-21 03:15:43 +0200

    testing

--- src/test/java/com/example/kafka101/Kafka101ApplicationTests.java
+++ src/test/java/com/example/kafka101/Kafka101ApplicationTests.java
@@ -1,15 +1,36 @@
 package com.example.kafka101;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
+import org.springframework.kafka.test.EmbeddedKafkaBroker;
 import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
+
+import java.util.concurrent.ExecutionException;
 
 @SpringBootTest
-@EmbeddedKafka
+@EmbeddedKafka(topics = "salah",
+        bootstrapServersProperty = "spring.kafka.bootstrap-servers")
+@DirtiesContext
 class Kafka101ApplicationTests {
+
+    @Autowired
+    private KafkaTemplate<String, String> template;
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testSendMessage() throws ExecutionException, InterruptedException {
+        ListenableFuture<SendResult<String, String>>  send = template.send("salah", "ah ha");
+        System.out.println(send.get().toString());
     }
 
 }
